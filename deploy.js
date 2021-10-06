71125722861a64d6d9685f72df898cd50bee8f05
fauky <fmented@gmail.com>
2021-10-07 00:08:41 +0700

    a

---
+++ deploy.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'build', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/fmented/ak-clone.git', // Update to point to your repository  
+        user: {
+            name: 'fmented', // update to use your name
+            email: 'fmented@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
