73f593692d56cdbf2112513c4df274deb7ab98d4
fauky <fmented@gmail.com>
2021-10-11 17:26:54 +0700

    tst api 2

--- src/hooks.js
+++ src/hooks.js
@@ -2,7 +2,7 @@ import { parseCookie, getUserFromToken } from "$lib/scripts/helper";
 
 
 export function getSession({headers}){
-    const cookie = headers.cookie
+    const cookie = headers.cookie || ''
     let token = parseCookie(cookie).userToken
 
     let user = getUserFromToken(token)
